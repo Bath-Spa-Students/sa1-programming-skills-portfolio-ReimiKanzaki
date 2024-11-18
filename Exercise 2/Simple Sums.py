d4f8a1ff4b80a059bb251c9756a439fdf25b6268
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-18 12:19:54 +0400

    Days Of The Month

---
+++ Exercise 2/Simple Sums.py
@@ -0,0 +1,7 @@
+#Simple Sums
+
+a = 8
+b = 10
+c = a + b
+
+print(c)
---
+++ Exercise 3/Biography.py
@@ -0,0 +1,7 @@
+#Biography
+
+Biography = {"Name" : 'Vicfirth Garcia' , 
+             "Hometown" : 'Sharjah, UAE' ,
+            "Age" : 19}
+
+print(Biography.items())
