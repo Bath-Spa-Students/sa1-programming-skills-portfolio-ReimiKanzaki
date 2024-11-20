ce2da97b16676e0fdf5fe0c3035e8602f2981f6a
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-20 21:28:13 +0400

    Hello

---
+++ Exercise 9/Hello.py
@@ -0,0 +1,14 @@
+#Hello
+
+#Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+
+
+def hello():
+    print("Hello")
+
+
+def main():
+ hello()
+
+if hello == main():
+    main()
