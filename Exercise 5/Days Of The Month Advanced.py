187e4c0104e7e55b7e1f2a4e5bb44d2583162e18
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-20 21:38:23 +0400

    Days Of The Month Advanced

---
+++ Exercise 5/Days Of The Month Advanced.py
@@ -0,0 +1,45 @@
+#Days Of The Month Advanced
+
+month = {"1" : 31 , 
+         "2" : 28 , 
+         "3" : 31 , 
+         "4" : 30 , 
+         "5" : 31 , 
+         "6" : 30 , 
+         "7" : 31 , 
+         "8" : 31 , 
+         "9" : 30 , 
+         "10" : 31 , 
+         "11" : 30 , 
+         "12" : 31}
+
+Month = str(input("Enter Month Number: "))
+
+if Month == "1":
+    print(month["1"])
+elif Month == "2":
+    leap_year = str(input("Leap Year?: "))
+    if leap_year.lower() == "yes":
+        print("29")
+    else:
+        print(month["2"])
+elif Month == "3":
+    print(month["3"])
+elif Month == "4":
+    print(month["4"])
+elif Month == "5":
+    print(month["5"])
+elif Month == "6":
+    print(month["6"])
+elif Month == "7":
+    print(month["7"])
+elif Month == "8":
+    print(month["8"])
+elif Month == "9":
+    print(month["9"])
+elif Month == "10":
+    print(month["10"])
+elif Month == "11":
+    print(month["11"])
+elif Month == "12":
+    print(month["12"])
