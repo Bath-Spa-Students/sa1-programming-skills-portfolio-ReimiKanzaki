29bc7eff7edebc85551877dedc8e8df43ffdd6c6
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-20 21:30:59 +0400

    Primitive Quiz Advanced

---
+++ Exercise 4/Primitive Quiz Advanced.py
@@ -0,0 +1,8 @@
+#Primitive Advanced
+
+Capital = str(input("What is the Capital of France?: "))
+
+if Capital.lower() == "paris":
+    print("The answer is correct")
+else:
+    print("The answer is incorrect")
