f78fbd92836d9c13133a3af64cc4789e942ebbae
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-21 19:25:13 +0400

    Update Biography Advanced.py

--- Exercise 3/Biography Advanced.py
+++ Exercise 3/Biography Advanced.py
@@ -4,7 +4,4 @@ Biography = {'name': str(input("Name?: ")),
            'address': str(input("Address?: ")),
            'age': str(input("Age?: "))} 
 
-print("\n Your Biography")
-print("Name:" +  " " + Biography["name"])
-print("Address:" +  " " + Biography["address"])
-print("Age:" +  " " + Biography["age"])
+print("\nName:" +  " " + Biography["name"], "\nAge:" +  " " + Biography["age"], "\nAddress:" +  " " + Biography["address"])
