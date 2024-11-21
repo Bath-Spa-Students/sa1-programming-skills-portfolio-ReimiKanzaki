463b0954e53a0ccbdcceeadbdc20c90bf954f709
Vicfirth Garcia <vicfirthpgarcia0331@gmail.com>
2024-11-21 18:10:51 +0400

    Biography Advanced

---
+++ Exercise 3/Biography Advanced.py
@@ -0,0 +1,10 @@
+#Biography Advanced
+
+Biography = {'name': str(input("Name?: ")),
+           'address': str(input("Address?: ")),
+           'age': str(input("Age?: "))} 
+
+print("\n Your Biography")
+print("Name:" +  " " + Biography["name"])
+print("Address:" +  " " + Biography["address"])
+print("Age:" +  " " + Biography["age"])
